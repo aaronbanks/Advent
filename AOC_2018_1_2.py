b610b85d7f781e69c4101aa67e0a0684edc3daad
aaronbanks <aaronbanks@aaronbanks.ca>
2021-12-14 15:39:17 -0500

    done

--- AOC_2018_1_2.py
+++ AOC_2018_1_2.py
@@ -1,25 +1,37 @@
-from functions import read_lines
+from AOC_Functions import read_lines
 
 def main():
 
     frequency = 0
+    set_of_frequencies = {0}
+    first_double = 0
 
     input = read_lines("input201801.txt")
 
-    for line in input:
+    while True:
+        for line in input:
 
-        if line[0]  == "+":
-            modifier_is_positive = True
-        elif line[0] == "-":
-            modifier_is_positive = False
+            if line[0]  == "+":
+                modifier_is_positive = True
+            elif line[0] == "-":
+                modifier_is_positive = False
 
-        line_value = line[1:]
+            line_value = line[1:]
 
-        if modifier_is_positive == True:
-            frequency += int(line_value)
-        if modifier_is_positive == False:
-            frequency -= int(line_value)
+            if modifier_is_positive == True:
+                frequency += int(line_value)
+            if modifier_is_positive == False:
+                frequency -= int(line_value)
 
-    print(frequency)
+            if frequency in set_of_frequencies:
+                first_double = frequency
+                break
+
+            set_of_frequencies.add(frequency)
+
+        if first_double == frequency:
+            break
+
+    print(first_double)
 
 main()
