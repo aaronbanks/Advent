8816b82bcfb0bb7a5a38b2d18dc6b17aaa71ba37
aaronbanks <aaronbanks@aaronbanks.ca>
2021-12-14 21:30:29 -0500

    go

--- AOC_2018_2_2.py
+++ AOC_2018_2_2.py
@@ -2,36 +2,54 @@ from AOC_Functions import read_lines
 
 def main():
 
-    # checksum = 0
-    # boxes_with_two_of_same = 0
-    # boxes_with_three_of_same = 0
-
     input = read_lines("input201802.txt")
 
+    shared_characters = ""
     for line in input:
-    #
-    #     set_of_characters_counted = set()
-    #
-    #     box_has_two_of_same = False
-    #     box_has_three_of_same = False
-    #
-    #     for character in line:
-    #         if character in set_of_characters_counted:
-    #             continue
-    #
-    #         set_of_characters_counted.add(character)
-    #
-    #         counter = line.count(character)
-    #
-    #         if counter == 2 and box_has_two_of_same == False:
-    #             boxes_with_two_of_same += 1
-    #             box_has_two_of_same = True
-    #
-    #         if counter == 3 and box_has_three_of_same == False:
-    #             boxes_with_three_of_same += 1
-    #             box_has_three_of_same == True
-    #
-    # checksum = boxes_with_two_of_same * boxes_with_three_of_same
-    # print(checksum)
+
+        box_pair_found = False
+
+        current_line_content = []
+
+        for character in line:
+            current_line_content.append(character)
+
+        for lines in input:
+
+            if lines == line:
+                continue
+
+            number_of_differences = 0
+            list_of_shared_characters = []
+
+            character_counter = -1
+
+            for characters in lines:
+
+                character_counter += 1
+
+                if characters == current_line_content[character_counter]:
+                    list_of_shared_characters.append(characters)
+                else:
+                    number_of_differences += 1
+
+            if number_of_differences == 1:
+                shared_characters = "".join(list_of_shared_characters)
+                print(shared_characters)
+                box_pair_found = True
+                break
+
+        if box_pair_found == True:
+            break
+
+main()
+
+
+
+
+
+
+
+
 
 main()
