882e389170712d08292c2750f34fff323c5cd2aa
aaronbanks <aaronbanks@aaronbanks.ca>
2021-12-15 14:59:59 -0500

    go

--- src/2019/2_1.py
+++ src/2019/2_1.py
@@ -4,9 +4,7 @@ def main():
 
     input = read_lines("./inputs/201902.txt")
 
-    intcode = input[0].split(",")
-
-    intcode = list(map(int, intcode))
+    intcode = list(map(int, input[0].split(",")))
 
     # intcode[1] = 12
     # intcode[2] = 2
@@ -17,16 +15,16 @@ def main():
     loop_counter = 0
 
     while True:
-    #ERROR IS OCCURING ON LOOP 3 WHEN THE FIRST MULTIPLICATION OCCURS, THE #19 STORED IN SLOT 19 SHOULD BE REPLACED WITH A 10, BUT IT'S NOT
+
         loop_counter += 1
 
         opcode = intcode[location_counter]
 
         if opcode == 1:
-            intcode[intcode[location_counter + 3]] = intcode[location_counter + 1] + intcode[location_counter + 2]
+            intcode[intcode[location_counter + 3]] = intcode[intcode[location_counter + 1]] + intcode[intcode[location_counter + 2]]
 
         elif opcode == 2:
-            intcode[intcode[location_counter + 3]] = intcode[location_counter + 1] * intcode[location_counter + 2]
+            intcode[intcode[location_counter + 3]] = intcode[intcode[location_counter + 1]] * intcode[intcode[location_counter + 2]]
 
         elif opcode == 99:
             break
