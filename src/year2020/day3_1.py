30bcf865042aacce0f2e04af2fcd2e495a61c730
aaronbanks <aaronbanks@aaronbanks.ca>
2021-12-20 13:02:34 -0500

    go

---
+++ src/year2020/day3_1.py
@@ -0,0 +1,62 @@
+from ..functions import run_solution
+
+input_filename = "./inputs/202003.txt"
+expected_output = None
+
+
+
+
+
+
+def solution(input_lines):
+    Trees_encountered_on_path = 0
+
+    length_of_input_lines
+
+    number_of_lines = 0
+
+
+
+
+
+    distance_to_bottom = 0
+
+    for line in input_lines:
+        distance_to_bottom +1
+
+    slope = (3, 1)
+
+    horizontal_distance_traveled = distance_to_bottom * slope[0]
+
+    line_multiplier = 1
+    length_of_input_lines = 0
+    first_line = True
+
+    for line in input_lines
+
+        if first_line == True:
+            for character in line:
+                length_of_lines += 1
+            first_line = False
+
+        while True:
+            line_multiplier += 1
+            if line_multiplier * length_of_input_lines >= horizontal_distance_traveled:
+                break
+
+        line.append(line * (line_multiplier - 1)
+
+
+
+    for current_line in input_lines:
+        pass
+
+    return
+
+
+
+
+
+
+if __name__ == "__main__":
+    run_solution(solution, input_filename, expected_output)
