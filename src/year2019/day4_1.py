0e3a44191a313f2603b443e1754ca127a36ec718
aaronbanks <aaronbanks@aaronbanks.ca>
2022-03-01 14:22:00 -0500

    go

---
+++ src/year2019/day4_1.py
@@ -0,0 +1,31 @@
+from ..functions import run_solution
+
+input_filename = "./inputs/202104.txt"
+expected_output = None
+
+problem_input = 367479-893698
+
+def solution(problem_input):
+
+
+    #password, 
+    # 6 digit number, 
+    # value in input range, 
+    # two adjacent digits are the same
+    #Left to right digits never decrease, only increase or stay the same
+
+#check each number in the range, see if it meets the avove criteria. 
+#Write a function for each check
+
+    for current_line in input_lines:
+        pass
+
+    return
+
+
+def other_function():
+    pass
+
+
+if __name__ == "__main__":
+    run_solution(solution, input_filename, expected_output)
